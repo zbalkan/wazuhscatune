31ce05cf62e880c359dea3cb6f4eadaf2f7449c5
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-05 23:48:06 +0300

    Cover Markdown structure escaping
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

---
+++ sca/tests/test_markdown_escape.py
@@ -0,0 +1,14 @@
+from sca.internal.guide import escape_markdown
+
+
+def test_markdown_structure_characters_are_escaped():
+    value = '# heading\n---\n- item\n> quote\n*em* _em_ `code`'
+    escaped = escape_markdown(value)
+
+    assert '\\# heading' in escaped
+    assert '\\-\\-\\-' in escaped
+    assert '\\- item' in escaped
+    assert '\\> quote' in escaped
+    assert '\\*em\\*' in escaped
+    assert '\\_em\\_' in escaped
+    assert '\\`code\\`' in escaped
