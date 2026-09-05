998ff954a4a096250b6b4df515a35c55f7215ade
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-05 23:55:07 +0300

    Fix Markdown blockquote escape test
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- sca/tests/test_markdown_escape.py
+++ sca/tests/test_markdown_escape.py
@@ -8,7 +8,7 @@ def test_markdown_structure_characters_are_escaped():
     assert '\\# heading' in escaped
     assert '\\-\\-\\-' in escaped
     assert '\\- item' in escaped
-    assert '\\> quote' in escaped
+    assert '&gt; quote' in escaped
     assert '\\*em\\*' in escaped
     assert '\\_em\\_' in escaped
     assert '\\`code\\`' in escaped
